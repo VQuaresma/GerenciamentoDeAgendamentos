0597eb3766593a8fbd15b62e4cf525ce04b06a2e
Vitor Quaresma <vitorquaresmadasilva@gmail.com>
2025-08-22 11:59:55 -0300

    configurações e acrescento de sistema de login

--- agendamento/views.py
+++ agendamento/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import HomeSerializer
+from .serializers import serializers
 
-# Create your views here.
+class HomeViewSet(viewsets.ViewSet):
+    def list(self, request):
+        data = {"message": "Bem-vindo à homepage!"}
+        serializer = HomeSerializer(data)
+        return Response(serializer.data)
